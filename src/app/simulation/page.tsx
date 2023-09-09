446ced777cfb8b935b2fa56337f5135e447bbccf
juancolchete <juanudkp@gmail.com>
2023-09-09 19:25:14 -0300

    feat: add simulation and send transaction

--- src/app/simulation/page.tsx
+++ src/app/simulation/page.tsx
@@ -4,7 +4,7 @@ import { RootState } from "@/store/store";
 import { ethers } from "ethers";
 import contracts from "../../contracts.json";
 import { useState } from "react";
-import { hexToString } from "@/utils/data";
+import { encodeToBase, decodeFromBase } from "@/utils/data";
 
 export default function Home() {
   const wallet = useSelector(
@@ -32,18 +32,22 @@ export default function Home() {
 
     let rawTransaction = await signer.signTransaction(transaction);
     let leadingZeros = rawTransaction?.match(/^0*/)?.[0]?.length;
-    let encodedRaw = hexToString(rawTransaction,parseInt(`${leadingZeros}`))
-    const txnRawEnc = `${leadingZeros},${chainId},${encodedRaw}` 
+    let encodedRaw = encodeToBase(BigInt(rawTransaction))
+    const txnRawEnc = `${leadingZeros},${chainId},${encodedRaw}`
+    const decodedRaw = decodeFromBase(encodedRaw,parseInt(`${leadingZeros}`))
     setTxnRaw(txnRawEnc)
     console.log("raw",rawTransaction)
-    console.log("txn",txnRawEnc)
+    console.log("txnDec",decodedRaw)
+    console.log("integrity",rawTransaction == decodedRaw)
+    console.log("integrity",encodedRaw)
+    console.log("integrity",encodedRaw.length)
   }
 
   return (
      <>
       {wallet.address}
       {wallet && wallet.loaded == true ?
-      <button onClick={() => getRawErc20('0x1643E812aE58766192Cf7D2Cf9567dF2C37e9B7F',"1","0xdC0cD343ebe20cA2ed01986518c3317F72f30447",5,wallet.nonceLIDO)}></button>
+      <button onClick={() => getRawErc20('0x1643E812aE58766192Cf7D2Cf9567dF2C37e9B7F',"1","0xdC0cD343ebe20cA2ed01986518c3317F72f30447",5,wallet.nonceLIDO+20)}></button>
       :<></>}
       <a href={`sms:+16152859912?&body=`+txnRaw}>[link text]</a>
     </> 
--- src/utils/data.ts
+++ src/utils/data.ts
@@ -2,21 +2,23 @@
   hex = hex.substring(2+leadingZeros) // remove the '0x' part
   var string = ""
 
-  while (hex.length % 8 != 0) { // we need it to be multiple of 8
+  while (hex.length % 4 != 0) { // we need it to be multiple of 8
     hex =  "0" + hex;
   }
 
   for (var i = 0; i < hex.length; i+= 8){
     string += String.fromCharCode(parseInt(hex.substring(i,i + 4), 16), parseInt(hex.substring(i + 4,i + 8), 16))
+    console.log(`string-${String.fromCharCode(parseInt(hex.substring(i,i + 4), 16), parseInt(hex.substring(i + 4,i + 8), 16))}-${parseInt(hex.substring(i,i + 4), 16), parseInt(hex.substring(i + 4,i + 8), 16)}`)
   }
-
-  return string.replaceAll(String.fromCharCode(0),"j");
-}
-
+  for (let i =0;i < 100;i++){
+    console.log(`${i}char-${String.fromCharCode(i)}`)
+  }
+  return string;
+  }
 function stringToHex(str:string,leadingZeros:number) {
-  const string = str.replaceAll("j",String.fromCharCode(0))
+  const string = str  
   var hex = ""
-  for (var i=0; i < string.length; i++) {
+  for (var i=0; i < string.length; i++){ 
     hex += ( (i == 0 ? "" : "000") + string.charCodeAt(i).toString(16)).slice(-4) // get character ascii code and convert to hexa string, adding necessary 0s
   }
   let leading = "";
@@ -25,5 +27,43 @@ function stringToHex(str:string,leadingZeros:number) {
   }
   return '0x'+ leading + hex;
 }
-export { hexToString,stringToHex };
+const customBase133 = {
+  characters: `0123456789@£$¥èéùìòÇØøÅåΔ_ΦΓΛΩΠΨΣΘΞÆæßÉ !#%&()*+-./:;<=>?ABCDEFGHIJKLMNOPQRSTUVWXYZÄÖÑÜ§¿abcdefghijklmnopqrstuvwxyzäöñüà¤|[]{}~^`,
+  base: BigInt(128),
+};
+function encodeToBase(number:bigint) {
+  const { characters, base } = customBase133;
+  let result = '';
+
+  while (number > BigInt(0)) {
+    const remainder = number % base;
+    result = characters[Number(remainder)] + result;
+    number = number / base;
+  }
+
+  return result;
+}
+
+function decodeFromBase(encoded:string,leadingZeros:number) {
+  const { characters, base } = customBase133;
+  let result = BigInt(0);
+
+  for (let i = 0; i < encoded.length; i++) {
+    const char = encoded.charAt(i);
+    const charValue = BigInt(characters.indexOf(char));
+    if (charValue === BigInt(-1)) {
+      throw new Error(`Invalid character "${char}" in the encoded string.`);
+    }
+    result = result * base + charValue;
+  }
+  let leading = "";
+  for(let i=0;i<leadingZeros;i++){
+    leading += "0"
+  }
+  return '0x'+ leading +result.toString(16);
+}
+const dec = "åjh5@ièä(X14CU/H4$4Q¤ß2lΨΣß*(ΩT_ÅE[({ÑWgÅ¥}ΓGhå4ßæ£ΛeH0000000000001d6;pFg8_ΞüH:6!k6.Gdf-ΞΛ00000000000000000000000$ü*Π [81äX000Δ853#Γ£ò9æa]0GCÖV3ÅÜbΠìPmΓΔ>ap4K|d£vD4£ΦÖ6Afå6v£éÜ2Πz(hØàÇ$CSDü<£nö0k^~&ΠKauàì";
+console.log("len",dec.length)
+console.log("decoded",decodeFromBase(dec,1))
+export { hexToString,stringToHex,encodeToBase,decodeFromBase };
 
